09bdd8e1d282efaf50f78c305d50eb48cdf394e8
Pat Myron <pat.myron@stedi.com>
2022-02-16 22:32:04 -0800

    AWS::KMS::Key.EnableKeyRotation

---
+++ aws/type/KMS/Key/EnableKeyRotation/src/kms_key_enablekeyrotation/handlers.py
@@ -0,0 +1,40 @@
+import logging
+from typing import Any, MutableMapping, Optional
+
+from cloudformation_cli_python_lib import (
+    HandlerErrorCode,
+    Hook,
+    HookInvocationPoint,
+    OperationStatus,
+    ProgressEvent,
+    SessionProxy,
+)
+
+from .models import HookHandlerRequest, TypeConfigurationModel
+
+# Use this logger to forward log messages to CloudWatch Logs.
+LOG = logging.getLogger(__name__)
+TYPE_NAME = "KMS::Key::EnableKeyRotation"
+
+hook = Hook(TYPE_NAME, TypeConfigurationModel)
+test_entrypoint = hook.test_entrypoint
+
+
+@hook.handler(HookInvocationPoint.CREATE_PRE_PROVISION)
+def pre_create_handler(
+        session: Optional[SessionProxy],
+        request: HookHandlerRequest,
+        callback_context: MutableMapping[str, Any],
+        type_configuration: TypeConfigurationModel
+) -> ProgressEvent:
+    try:
+        request.hookContext.targetModel['resourceProperties']['EnableKeyRotation']
+        return ProgressEvent(
+            status = OperationStatus.SUCCESS
+        )
+    except KeyError:
+        return ProgressEvent(
+            status = OperationStatus.FAILED,
+            message = 'property required by hook',
+            errorCode = HandlerErrorCode.NonCompliant,
+        )
