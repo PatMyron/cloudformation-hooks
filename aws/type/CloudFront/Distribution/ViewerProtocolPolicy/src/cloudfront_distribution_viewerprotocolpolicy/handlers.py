de9d5f5021f27794ad665c7339e31ab74c9eab9a
Pat Myron <pat.myron@stedi.com>
2022-02-16 23:24:05 -0800

    AWS::CloudFront::Distribution.DistributionConfig.DefaultCacheBehavior.ViewerProtocolPolicy

---
+++ aws/type/CloudFront/Distribution/ViewerProtocolPolicy/src/cloudfront_distribution_viewerprotocolpolicy/handlers.py
@@ -0,0 +1,40 @@
+import logging
+from typing import Any, MutableMapping, Optional
+
+from cloudformation_cli_python_lib import (
+    HandlerErrorCode,
+    Hook,
+    HookInvocationPoint,
+    OperationStatus,
+    ProgressEvent,
+    SessionProxy,
+)
+
+from .models import HookHandlerRequest, TypeConfigurationModel
+
+# Use this logger to forward log messages to CloudWatch Logs.
+LOG = logging.getLogger(__name__)
+TYPE_NAME = "CloudFront::Distribution::ViewerProtocolPolicy"
+
+hook = Hook(TYPE_NAME, TypeConfigurationModel)
+test_entrypoint = hook.test_entrypoint
+
+
+@hook.handler(HookInvocationPoint.CREATE_PRE_PROVISION)
+def pre_create_handler(
+        session: Optional[SessionProxy],
+        request: HookHandlerRequest,
+        callback_context: MutableMapping[str, Any],
+        type_configuration: TypeConfigurationModel
+) -> ProgressEvent:
+    try:
+        request.hookContext.targetModel['resourceProperties']['DistributionConfig']['DefaultCacheBehavior']['ViewerProtocolPolicy']
+        return ProgressEvent(
+            status = OperationStatus.SUCCESS
+        )
+    except KeyError:
+        return ProgressEvent(
+            status = OperationStatus.FAILED,
+            message = 'property required by hook',
+            errorCode = HandlerErrorCode.NonCompliant,
+        )
